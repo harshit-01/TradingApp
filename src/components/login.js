f558973b941cc0bdd28f0d098d49f4c626c5eb73
Harshit Kashyap <harshit.kashyap@nokia.com>
2023-11-09 18:10:07 +0530

    Functionality update and other changes

--- src/components/login.js
+++ src/components/login.js
@@ -4,13 +4,54 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import { useNavigate  } from "react-router-dom";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef,forwardRef } from "react";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import axios from 'axios';
+
+const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+  
 
 export default function Signup(){
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
     const [usernameError, setUsernameError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
+    const [openIncorrectDetails, setOpenIncorrectDetails] = useState(false);
+    const [openSuccess, setOpenSuccess] = useState(false);
+    const [open, setOpen] = useState(false);
+    const handleClickSnackbar = () => {
+        setOpen(true);
+    };
+    const handleSuccessClickSnackbar = () => {
+        setOpen(true);
+    };
+    const handleIncorrectClickSnackbar = () => {
+        setOpenIncorrectDetails(true);
+    };
+    const handleCloseIncorrectSnackbar = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+
+        setOpenIncorrectDetails(false);
+    };
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+
+        setOpen(false);
+    };
+    const handleCloseSuccess = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+
+        setOpenSuccess(false);
+    };
     let navigate = useNavigate();
     const handleUsernameChange = (e)=> {
         setUsername(e.target.value);
@@ -18,7 +59,7 @@ export default function Signup(){
     const handlePasswordChange = (e)=> {
         setPassword(e.target.value);
     }
-    const handleClick = (e) => {
+    const handleClick = async(e) => {
         if(username.length === 0){
             setUsernameError(true);
             if(password.length === 0){
@@ -38,7 +79,27 @@ export default function Signup(){
                 setPasswordError(false);
             }
         }
-        return navigate("/home");
+        await axios.post("http://localhost:8081/auth",{username,password}).then((res)=>{
+            console.log(res);
+            console.log(res?.data?.response?.split(",")[1]?.split(":")[2]?.split('"')[1])
+            if(res.status == "200"){
+                handleSuccessClickSnackbar();
+                localStorage.setItem("username", username);
+                localStorage.setItem("userId",res.data.response.split(",")[1].split(":")[2].split('"')[1])
+                return navigate("/home");
+            }
+           
+        }).catch((err)=>{
+            console.log(err)
+            if(err?.response?.status == "401"){
+                console.log(openIncorrectDetails)
+                handleIncorrectClickSnackbar();
+            }
+            if(err?.response?.status =="404"){
+                handleClickSnackbar();
+            }
+            console.log(err)
+        })
         console.log(username,password,usernameError);
         // API request
     };
@@ -74,6 +135,21 @@ export default function Signup(){
                     </Link>
                 </div>
             </div>
+            <Snackbar anchorOrigin={{ vertical:"top", horizontal:"right" }} open={openSuccess} autoHideDuration={6000} onClose={handleCloseSuccess}>
+                <Alert onClose={handleCloseSuccess} severity="error" sx={{ width: '100%' }}>
+                    User Logged in successfully;
+                </Alert>
+            </Snackbar>
+            <Snackbar anchorOrigin={{ vertical:"top", horizontal:"right" }} open={openIncorrectDetails} autoHideDuration={6000} onClose={handleCloseIncorrectSnackbar}>
+                <Alert onClose={handleCloseIncorrectSnackbar} severity="error" sx={{ width: '100%' }}>
+                    Incorrect credentials provided
+                </Alert>
+            </Snackbar>
+            <Snackbar anchorOrigin={{ vertical:"top", horizontal:"right" }} open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                    User Not found in the database
+                </Alert>
+            </Snackbar>
         </div>
     )
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,29 +1,66 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState,forwardRef } from "react"
 import Navbar from "./navbar.js"
 import Button from '@mui/material/Button';
 import { useNavigate  } from "react-router-dom";
 import TextField from '@mui/material/TextField';
+import ProfilePic from "../images/profilePic.png"
+import ProfileDialog from "./profileModal.js"
+import axios from "axios"
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+  
 
 export default function Profile(){
     let navigate = useNavigate();
     const [open,setOpen] = useState();
     const [password,setPassword] = useState("");
+    const [openModal,setOpenModal] = useState(false);
+    const [userDetails,setUserDetails] = useState(null);
+    const [openSnackbar,setOpenSnackbar] = useState(false)
     let s = "";
     const handlePasswordChange = (e)=>{
         s = s + e.target.value;
         setPassword(s);
     }
+    const handleCloseSuccess = (event, reason)=>{
+        if (reason === 'clickaway') {
+            return;
+            }
+    
+        setOpenSnackbar(false);
+    }
+    async function getUserDetail(){
+        let id = localStorage.getItem("userId");
+        console.log(id);
+        let response = axios.get(`http://localhost:8081/userDetailsById/${id}`,)
+            .then((res)=>{
+                setUserDetails(res.data);
+                console.log(res);
+            }).catch((err)=>{
+                console.log(err);
+            })
+    }
+    useEffect(()=>{
+        getUserDetail();
+      },[])
+    console.log(openModal)
     return(
         <>
             <Navbar />
             <div className="" style={{margin:"15px"}}>
                 <div style={{display:"flex",justifyContent:"center"}}>
-                    <div style={{height:"100px",width:"100px",border:"1px solid grey",borderRadius:"5px"}}></div>
+                    <div style={{height:"100px",width:"100px",border:"1px solid grey",borderRadius:"5px"}}>
+                        <img src={ProfilePic} width="90px" height="90px" style={{padding:"5px"}}></img>
+                    </div>
                 </div>
                 <div>
-                    Name
+                    <b>{userDetails ? userDetails.name : null}</b>
                 </div>
-                <div style={{display:"flex",justifyContent:"flex-start",alignItems:"center",margin:"35px"}}>
+                <div style={{display:"flex",justifyContent:"flex-start",alignItems:"center",margin:"35px",fontWeight:"bold"}}>
                     <div style={{width:"100%"}}>
                         Phone No.
                     </div>
@@ -39,19 +76,20 @@ export default function Profile(){
                 </div>
                 <div style={{display:"flex",justifyContent:"flex-start",alignItems:"center",margin:"35px"}}>
                     <div style={{width:"100%"}}>
-                        Na
+                    {userDetails ? userDetails.number : "NA"}
                     </div>
                     <div style={{width:"100%"}}>
-                        Na
+                    {userDetails ? userDetails.id : "NA"}
                     </div>
                     <div style={{width:"100%"}}>
-                        Na
+                    {userDetails ? userDetails.address : "NA"}
                     </div>
                     <div style={{width:"100%"}}>
-                        Na
+                        0
                     </div>
                 </div>
                 <Button variant="outlined" onClick={()=>setOpen(!open)}>Add funds</Button>
+                <Button variant="outlined" style={{marginLeft:"15px"}} onClick={()=>setOpenModal(true)}>Edit profile</Button>
                 {open ? 
                 <div style={{margin:"50px",padding:"15px",display:"flex",justifyContent:"center",alignItems:"center",
                 boxShadow:"1px 1px 1px 1px grey"}}>
@@ -59,7 +97,13 @@ export default function Profile(){
                     <TextField value={password} onChange={handlePasswordChange}  
                     size="small" fullWidth label="Password" id="fullWidth" />
                 </div>: null}
+                <ProfileDialog openModal={openModal} setOpenModal={setOpenModal} getUserDetail={getUserDetail} setOpenSnackbar={setOpenSnackbar} handleCloseSuccess={handleCloseSuccess}/>
             </div>
+            <Snackbar anchorOrigin={{ vertical:"top", horizontal:"right" }} open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSuccess}>
+                <Alert onClose={handleCloseSuccess} severity="success" sx={{ width: '100%' }}>
+                    User Logged in successfully;
+                </Alert>
+            </Snackbar>
         </>
     )
 }
